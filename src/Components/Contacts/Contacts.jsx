2592db39c990dd983c2eeed6a9d92adcbd2a3844
aliha <alihajji315@gmail.com>
2024-04-04 13:16:42 +0000

    first commit

---
+++ src/Components/Contacts/Contacts.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+ class Contacts extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          COntacts
+        </h1>
+        <ul>
+          <li>ali hajji </li>
+          <li>0687442497</li>
+        </ul>
+      </div>
+    )
+  }
+  
+}
+export default Contacts;
